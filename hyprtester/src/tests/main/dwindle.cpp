3e01666a2ec363a38cc9a6fe67bb08172d29e112
Vaxry <vaxry@vaxry.net>
2025-11-17 13:29:46 +0000

    add test

--- hyprtester/src/tests/main/dwindle.cpp
+++ hyprtester/src/tests/main/dwindle.cpp
@@ -32,7 +32,50 @@ static void testFloatClamp() {
     OK(getFromSocket("/keyword dwindle:force_split 0"));
 
     // clean up
+    OK(getFromSocket("/reload"));
+    OK(getFromSocket("/keyword monitor HEADLESS-2, addreserved, 0, 0, 0, 0"));
     NLog::log("{}Killing all windows", Colors::YELLOW);
+    Tests::killAllWindows();
+}
+
+static void testResize() {
+
+    getFromSocket("/dispatch workspace 1");
+
+    // force windows to split right
+    OK(getFromSocket("/keyword dwindle:force_split 2"));
+    OK(getFromSocket("/keyword dwindle:split_width_multiplier 0.001"));
+    OK(getFromSocket("/keyword general:gaps_in 0"));
+    OK(getFromSocket("/keyword general:gaps_out 0"));
+    OK(getFromSocket("/keyword general:border_size 0"));
+
+    const auto HALF = 1920 / 2;
+    const auto QUARTER = 1920 / 4;
+
+    Tests::spawnKitty("kitty_a");
+    Tests::spawnKitty("kitty_b");
+    Tests::spawnKitty("kitty_c");
+
+    EXPECT(Tests::windowCount(), 3);
+
+    {
+        auto str = getFromSocket("/clients");
+        EXPECT_COUNT_STRING(str, std::format("size: {},1080", HALF), 1);
+        EXPECT_COUNT_STRING(str, std::format("size: {},1080", QUARTER), 2);
+    }
+
+    OK(getFromSocket("/dispatch focuswindow class:kitty_a"));
+    OK(getFromSocket("/dispatch resizeactive -50 0"));
+
+    {
+        auto str = getFromSocket("/clients");
+        EXPECT_COUNT_STRING(str, std::format("size: {},1080", HALF - 50), 1);
+        EXPECT_COUNT_STRING(str, std::format("size: {},1080", QUARTER), 1);
+        EXPECT_COUNT_STRING(str, std::format("size: {},1080", QUARTER + 50), 1);
+    }
+
+    NLog::log("{}Killing all windows", Colors::YELLOW);
+    OK(getFromSocket("/reload"));
     Tests::killAllWindows();
 }
 
@@ -42,6 +85,9 @@ static bool test() {
     // test
     NLog::log("{}Testing float clamp", Colors::GREEN);
     testFloatClamp();
+
+    NLog::log("{}Testing resize", Colors::GREEN);
+    testResize();
 
     // clean up
     NLog::log("Cleaning up", Colors::YELLOW);
